c9c68880d57a08b4babb68c3bf3303476b3729c6
pjm77 <probably.just.me77@gmail.com>
2018-05-09 23:59:11 +0200

    add message entity

--- src/main/java/pl/pjm77/entities/Message.java
+++ src/main/java/pl/pjm77/entities/Message.java
@@ -1,5 +1,28 @@
 package pl.pjm77.entities;
 
-public class Message {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="messages")
+public class Message {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private boolean unread;
+	
+	@OneToOne
+	@JoinColumn
+	private User sender;
+	
+	@OneToOne
+	@JoinColumn
+	private User receiver;
 }
