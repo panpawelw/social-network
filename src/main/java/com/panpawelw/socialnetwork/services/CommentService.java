ccac0fa5ee632d72d251939822306eecd280abbb
Paweł Wojtuń <panpawelw@pm.me>
2021-09-10 01:07:34 +0200

    Add CommentService

---
+++ src/main/java/com/panpawelw/socialnetwork/services/CommentService.java
@@ -0,0 +1,25 @@
+package com.panpawelw.socialnetwork.services;
+
+import com.panpawelw.socialnetwork.entities.Comment;
+import com.panpawelw.socialnetwork.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+  private final CommentRepository repository;
+
+  public CommentService(CommentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Comment> findAllByPostId(long id) {
+    return repository.findAllByPostIdOrderByCreatedDesc(id);
+  }
+
+  public long save(Comment comment) {
+    return repository.saveAndFlush(comment).getId();
+  }
+}
